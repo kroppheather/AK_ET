94f8e69eb3d82d3a46d253467c2d6d033ef0e164
kroppheather <kroppheather@gmail.com>
2020-07-08 10:57:09 -0400

    used another method to access and organize data

---
+++ data_download.r
@@ -0,0 +1,34 @@
+#starting script to organize data download
+# Install rhdf5 package (only need to run if not already installed)
+#install.packages("BiocManager")
+#BiocManager::install("rhdf5")
+#install.packages("neonUtilities")
+# Call the R HDF5 Library
+library(rhdf5)
+#neon library
+library(neonUtilities)
+options(stringsAsFactors=F)
+
+dirD <- "/Users/hkropp/Google Drive/research/Healy_ET/healy_flux
+
+zipsByProduct(dpID="DP4.00200.001", package="basic", 
+              site=c("HEAL"), 
+              startdate="2017-01", enddate="2020-05",
+              savepath=dirD, 
+              check.size=F)
+
+
+flux <- stackEddy(filepath=paste0(dirD,"/filesToStack00200"),
+                 level="dp04")
+
+plot(as.Date(flux$HEAL$timeEnd), flux$HEAL$data.fluxH2o.nsae.flux)
+
+tail(flux$HEAL)
+timeB <- as.POSIXct(flux$HEAL$timeEnd, 
+                    format="%Y-%m-%dT%H:%M:%S", 
+                    tz="GMT")
+flux$HEAL <- cbind(timeB, flux$HEAL)
+write.table(flux$HEAL,"/Users/hkropp/Google Drive/research/Healy_ET/healy_flux/healyFluxes.csv", sep=",", row.names=FALSE)
+
+write.table(flux$variables,"/Users/hkropp/Google Drive/research/Healy_ET/healy_flux/healyFluxesVariables.csv", sep=",", row.names=FALSE)
+
