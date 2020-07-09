c97805bd17500a223a811856565b943068007187
kroppheather <kroppheather@gmail.com>
2020-07-08 20:19:57 -0400

    worked out raster workflow

--- data_download.r
+++ data_download.r
@@ -8,27 +8,42 @@ library(rhdf5)
 #neon library
 library(neonUtilities)
 options(stringsAsFactors=F)
+library(raster)
 
 dirD <- "/Users/hkropp/Google Drive/research/Healy_ET/healy_flux"
 
 #zipsByProduct(dpID="DP4.00200.001", package="expanded", 
+ #             site=c("HEAL"), 
+  #            startdate="2017-01", enddate="2017-12",
+   #           savepath=paste0(dirD,"/2017"), 
+    #          check.size=F)
+
+#zipsByProduct(dpID="DP4.00200.001", package="expanded", 
 #              site=c("HEAL"), 
-#              startdate="2017-01", enddate="2020-05",
-#              savepath=dirD, 
+#              startdate="2018-08", enddate="2018-09",
+#              savepath=paste0(dirD,"/summer2018"), 
 #              check.size=F)
 
+#zipsByProduct(dpID="DP4.00200.001", package="expanded", 
+ #             site=c("HEAL"), 
+  #            startdate="2019-01", enddate="2019-12",
+   #           savepath=paste0(dirD,"/2019"), 
+    #          check.size=F)
 
 ##flux <- stackEddy(filepath=paste0(dirD,"/filesToStack00200"),
 #                 level="dp04")
 
-  foot <-   footRaster(filepath=paste0(dirD,"/filesToStack00200"))        
+  foot <-   footRaster(filepath=paste0(dirD,"/summer2018/filesToStack00200"))        
+str(foot[[1]])
+str(foot[[2]])
+str(foot[[3]])
+foot[[1]]@data@names
 
+foot[[2]]@data@names
 #plot(as.Date(flux$HEAL$timeEnd), flux$HEAL$data.fluxH2o.nsae.flux)
 
 #tail(flux$HEAL)
-#timeB <- as.POSIXct(flux$HEAL$timeEnd, 
-                    format="%Y-%m-%dT%H:%M:%S", 
-                    tz="GMT")
+
 #flux$HEAL <- cbind(timeB, flux$HEAL)
 #write.table(flux$HEAL,"/Users/hkropp/Google Drive/research/Healy_ET/healy_flux/healyFluxes.csv", sep=",", row.names=FALSE)
 
