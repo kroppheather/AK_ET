f07d3fcd8b06580007299a1f179fe0b33ab18006
kroppheather <kroppheather@gmail.com>
2020-07-24 07:55:43 -0400

    running second flight day

--- flir_process.R
+++ flir_process.R
@@ -90,10 +90,10 @@ flightDataR <- list()
 flightDF <- list()
 flightDF2 <- data.frame()
 lyseq <- seq(0,255)
-subl <- c(1,seq(20,220, by=20),255)
+subl <- c(2,seq(20,220, by=20),256)
 pcol <- character()
 #files are 120x 160 pixels
-for(i in 1:length(flightDIR)){
+for(i in 1){
   for(j in 1:length(flightFiles[[i]])){
     #read in data file and reorganize
     flightDataR[[j]]<- read.csv(paste0(imgDIR,"/",flightDIR[i],"/",flightFiles[[i]][j]),head=FALSE)
@@ -127,23 +127,84 @@ for(i in 1:length(flightDIR)){
     
     dev.off()	
   }
-  
-  
+} 
+for(i in 1){  
   #make a legend for the flight color and temp
-  jpeg(paste0(outDIR,"/",flightDIR[i],"_temperature_legend.jpg"),width=500,height=1000, units="px")
-  layout(matrix(c(1),ncol=1), width=c(lcm(5)), height=c(lcm(30)))	
-  
-  plot(c(0,1),c(0,1),  type="n", ylim=c(0,255),xlim=c(0,1), xaxs="i",yaxs="i",
+  jpeg(paste0(outDIR,"/",flightDIR[i],"_temperature_legend.jpg"),width=5,height=10, res=300,units="cm")
+  layout(matrix(c(1),ncol=1), width=c(lcm(1)), height=c(lcm(6)))	
+  par(mai=c(0,0,0,0))
+  plot(c(0,0.5),c(0,0.5),  type="n", ylim=c(0,256),xlim=c(0,1), xaxs="i",yaxs="i",
        xlab=" ", ylab=" ",axes=FALSE)
-  for(k in 1:(254)){
-    polygon(c(0,0,1,1), c(lyseq[k],lyseq[k]+1,lyseq[k]+1,lyseq[k]),col=ccol[k], border=NA)
-  }
+  points(rep(0.25,256), seq(0,255), col=dfN[[1]]$col,pch=15)
+
   
-  axis(4,lyseq[subl],dfN[[i]]$startT[subl],las=2,cex.axis=2)
+  axis(4,lyseq[subl],dfN[[i]]$startT[subl],las=2,cex.axis=1)
   dev.off()
 }	
 
 
 #write color info into dataframe
 
-write.table(dfN[[1]], paste0(outDIR,"/",flightDIR[i],"_temperature_key.jpg"))
+write.table(dfN[[1]], paste0(outDIR,"/",flightDIR[1],"_temperature_key.csv"), sep=",", row.names=FALSE)
+write.table(dfN[[2]], paste0(outDIR,"/",flightDIR[2],"_temperature_key.csv"), sep=",", row.names=FALSE)
+
+
+#since for loop stopped on error now run second flight directory
+
+
+#read in csvs and set up tiff for each one
+flightDataR <- list()
+flightDF <- list()
+flightDF2 <- data.frame()
+lyseq <- seq(0,255)
+subl <- c(2,seq(20,220, by=20),256)
+pcol <- character()
+#files are 120x 160 pixels
+for(i in 2){
+  for(j in 1:length(flightFiles[[i]])){
+    #read in data file and reorganize
+    flightDataR[[j]]<- read.csv(paste0(imgDIR,"/",flightDIR[i],"/",flightFiles[[i]][j]),head=FALSE)
+    
+    flightDF[[j]] <- data.frame(Y=rep(seq(1,120),each=160),X=rep(seq(1,160),times=120),
+                                Temp=as.vector(data.matrix(flightDataR[[j]])))
+    #assign a color for each pixel
+    
+    for(k in 1:dim(flightDF[[j]])[1]){
+      for(m in 1:dim(dfN[[i]])[1]){
+        if(flightDF[[j]]$Temp[k] >= dfN[[i]]$startT[m]&flightDF[[j]]$Temp[k] < dfN[[i]]$endT[m]){
+          pcol[k] <- dfN[[i]]$col[m]
+        }
+      }	
+      
+    }
+    flightDF[[j]]$pcol <- pcol
+    
+    jpeg(paste0(outDIR,"/",flightDIR[i],"/",gsub(".csv","",flightFiles[[i]][j]),".jpg"),
+         width=640,height=480, units="px")
+    
+    par(mai=c(0,0,0,0))	
+    plot(c(0,1),c(0,1), type="n", ylim=c(120,0),xlim=c(0,160), xaxs="i",yaxs="i",
+         xlab=" ", ylab=" ")
+    
+    for(k in 1:dim(flightDF[[j]])[1]){
+      polygon(c(flightDF[[j]]$X[k]-1,flightDF[[j]]$X[k]-1,flightDF[[j]]$X[k],flightDF[[j]]$X[k]),
+              c(flightDF[[j]]$Y[k]-1,flightDF[[j]]$Y[k],flightDF[[j]]$Y[k],flightDF[[j]]$Y[k]-1),
+              border=NA, col=flightDF[[j]]$pcol[k])
+    }			
+    
+    dev.off()	
+  }
+} 
+for(i in 2){  
+  #make a legend for the flight color and temp
+  jpeg(paste0(outDIR,"/",flightDIR[i],"_temperature_legend.jpg"),width=5,height=10, res=300,units="cm")
+  layout(matrix(c(1),ncol=1), width=c(lcm(1)), height=c(lcm(6)))	
+  par(mai=c(0,0,0,0))
+  plot(c(0,0.5),c(0,0.5),  type="n", ylim=c(0,256),xlim=c(0,1), xaxs="i",yaxs="i",
+       xlab=" ", ylab=" ",axes=FALSE)
+  points(rep(0.25,256), seq(0,255), col=dfN[[1]]$col,pch=15)
+  
+  
+  axis(4,lyseq[subl],dfN[[i]]$startT[subl],las=2,cex.axis=1)
+  dev.off()
+}	
