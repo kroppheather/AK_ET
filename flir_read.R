eaf6869cd7a14a2be81637592c9d777601162b7e
kroppheather <kroppheather@gmail.com>
2020-08-11 09:36:18 -0400

    working on thermal

--- flir_read.R
+++ flir_read.R
@@ -3,7 +3,11 @@ library(dplyr)
 key <- read.csv("/Users/hkropp/Google Drive/research/Healy_ET/alaska_2018/flir_out/tiff/7_04_temperature_key.csv")
 
 
-ortho <- raster("/Users/hkropp/Google Drive/research/Healy_ET/alaska_2018/odm_ortho/07_04/odm_orthophoto.tif")
+ortho <- raster("/Users/hkropp/Google Drive/research/Healy_ET/alaska_2018/odm_ortho/07_04/odm_ortho2_geo2.tif")
+plot(ortho)
+
+
+
 key$mid <- key$startT + ((key$endT - key$startT)/2)
 
 
@@ -13,4 +17,4 @@ othoA <- left_join(orthoV,key, by="DN")
 orthoT <- setValues(ortho,othoA$mid)
 plot(orthoT)
 
-plot(ortho)
+writeRaster(orthoT, "/Users/hkropp/Google Drive/research/Healy_ET/alaska_2018/odm_ortho/07_04/georefT_07_04.tif",overwrite=TRUE)
